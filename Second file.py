d37e16a8b8fe3b51f56941897e59b8e7c91a300d
Pavan Karasala <kpavan01@gmail.com>
2020-04-18 10:16:45 -0500

    Initial commit

---
+++ Second file.py
@@ -0,0 +1,32 @@
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
---
+++ whileexample.py
@@ -0,0 +1,37 @@
+From Pavan Karasala
+n = 25.5
+m = 26.7
+x = n + m
+print(x)
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line =='done':
+        break
+    print(line)
+print('Done')
